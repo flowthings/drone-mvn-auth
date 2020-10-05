85b7519c1a46bd0886cf6b89443ba3ce0139a175
germanSancho <german@gametube.org>
2020-10-05 12:32:06 +0200

    add build/tag/push scripts for dev and stage

---
+++ az_stage_build.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+set -e
+
+ACR_NAME='octavestage'
+REPO_NAME='platform_build_auth'
+SUBSCRIPTION='OCTAVE-STAGE'
+
+if [ $# -eq 0 ]
+  then
+    TAG=`git rev-parse --short HEAD`
+    echo "Using Git Tag $TAG"
+else
+    TAG=$1
+fi
+
+echo "Building, Tagging, and Pushing container with Tag: $TAG"
+
+az acr login --name $ACR_NAME --subscription $SUBSCRIPTION
+
+docker build -t $ACR_NAME.azurecr.io/$REPO_NAME:$TAG .
+
+docker push $ACR_NAME.azurecr.io/$REPO_NAME:$TAG
